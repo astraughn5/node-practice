7f871e990b2ca3440c7acb20f365f4ffeb240f93
astraughn5 <astraughn5@gmail.com>
2022-06-04 10:40:06 -0500

    fixing issue with POST

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
-
+app.use(express.json())
 
 const date = new Date
 let info = [
@@ -39,6 +39,41 @@ app.get('/api/info/:id', (req, res)=>{
 	else{
 		res.status(404).end()
 	}
+})
+
+const generateId = () => {
+	const maxId = notes.length > 0
+	  ? Math.max(...notes.map(n => n.id))
+	  : 0
+	return maxId + 1
+  }
+
+app.post('/api/info',(req, res)=>{
+	const data = req.body
+	
+	if (!body.content) {
+		return response.status(400).json({ 
+		  error: 'content missing' 
+		})
+	  }
+	
+	const info = {
+		content: body.content,
+		important: body.important || false,
+		date: date,
+		id: generateId(),
+	}
+	data = data.concat(info)
+	//console.log(data)
+	res.json(data)
+})
+
+app.delete('/api/info/:id', (req, res)=>{
+	const id = Number(req.params.id)
+	info = info.filter(data => data.id !== id)
+	
+	res.status(204).end()
+
 })
 
 app.listen(PORT, ()=>{
